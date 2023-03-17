0aa2404861125ccde97a729484fdd5b1551373d4
Arkadiusz <arkadiusz@jagura.net>
2023-03-17 10:05:20 +0100

    Introduce constant WEATHER_URL

--- src/main/java/net/jagura/weather/service/WeatherService.java
+++ src/main/java/net/jagura/weather/service/WeatherService.java
@@ -10,10 +10,11 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class WeatherService {
 
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/";
     private RestTemplate restTemplate = new RestTemplate();
 
     public WeatherDto getWeather() {
-        String response = restTemplate.getForObject("http://api.openweathermap.org/data/2.5/weather?q=brwinow&appid=fdf139e86da1b9b2bd61888b37bc6b86&units=metric&lang=pl",
+        String response = restTemplate.getForObject(WEATHER_URL + "weather?q=brwinow&appid=fdf139e86da1b9b2bd61888b37bc6b86&units=metric&lang=pl",
                 String.class);
         log.info(response);
         return null;
